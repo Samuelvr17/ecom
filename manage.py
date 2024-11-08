99f9dff554544132cb9d3d7584964ab838baf5ad
Samuel Vergara <samuelvergarar@gmail.com>
2024-11-08 16:44:22 -0500

    confing vercel

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import execute_from_command_line
 
 
 def main():
@@ -18,5 +19,6 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecom.settings")
+    execute_from_command_line(sys.argv)
