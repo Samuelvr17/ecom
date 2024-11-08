840fbf8125dc8225fe759cf20da0419b1aea5700
Samuel Vergara <samuelvergarar@gmail.com>
2024-11-08 16:30:37 -0500

    Configurando Vercel para desplegar la app

--- ecom/settings.py
+++ ecom/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static/']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 
 
 MEDIA_URL = 'media/'
